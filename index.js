a5a932e2c309910c055d050dfe517726b0f0b1f9
sumit245 <sumitranjan245@gmail.com>
2022-06-05 11:29:22 +0530

    Message

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require("path")
 
 const app = express();
-app.use(express.static(path.join(__dirname, "../build")))
+app.use(express.static(path.join(__dirname, "build")))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"))
+    res.sendFile(path.join(__dirname, "build", "index.html"))
 }
 )
 const PORT = process.env.PORT || 4000;
